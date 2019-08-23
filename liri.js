4698f5d0ca2b241a2f3e18e1de9db44ff330cba7
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-08-23 01:23:33 -0500

    search for concert info and log info to log.txt

--- liri.js
+++ liri.js
@@ -39,3 +39,68 @@ function switchCase() {
   }
 };
 
+//Concert search
+function bandsInTown(parameter){
+
+    if (action === 'concert')
+    {
+        var artisName="";
+        for (var i = 3; i < process.argv.length; i++)
+        {
+            artisName+=process.argv[i];
+        }
+        console.log(artisName);
+    }
+    else
+    {
+        artisName = parameter;
+    }
+    
+    
+    
+    var queryUrl = "https://rest.bandsintown.com/artists/"+artisName+"/events?app_id=codingbootcamp";
+    
+    
+    request(queryUrl, function(error, response, body) {
+    
+      if (!error && response.statusCode === 200) {
+    
+        var JS = JSON.parse(body);
+        for (i = 0; i < JS.length; i++)
+        {
+          var dTime = JS[i].datetime;
+            var month = dTime.substring(5,7);
+            var year = dTime.substring(0,4);
+            var day = dTime.substring(8,10);
+            var dateForm = month + "/" + day + "/" + year
+      
+          logIt("\n---------------------- "+artisName+" -----------------------------\n");
+    
+            
+          logIt("Date: " + dateForm);
+          logIt("Name: " + JS[i].venue.name);
+          logIt("City: " + JS[i].venue.city);
+          if (JS[i].venue.region !== "")
+          {
+            logIt("Country: " + JS[i].venue.region);
+          }
+          logIt("Country: " + JS[i].venue.country);
+          logIt("\n---------------------------------------------------\n");
+    
+        }
+      }
+    });
+    }
+
+    //log info to log.txt
+    function logIt(dataToLog) {
+
+        console.log(dataToLog);
+    
+        fs.appendFile('log.txt', dataToLog + '\n', function(err) {
+            
+            if (err) return logIt('Error logging data to file: ' + err);	
+        });
+    }
+    switchCase();
+
