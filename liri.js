a27259b2431965491ea44cdbc1d71f1b36b0a4bf
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-08-23 00:56:43 -0500

    set up basic common with switch condition

--- liri.js
+++ liri.js
@@ -1,3 +1,41 @@
 require("dotenv").config();
-var keys = require("./keys");
+var fs = require("fs");
+var keys = require("./keys.js");
+var request = require('request');
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+
+var action = process.argv[2];
+var parameter = process.argv.slice(3).join(" ");
+
+
+
+
+function switchCase() {
+
+  switch (action) {
+
+    case 'concert':
+      bandsInTown(parameter);                   
+      break;                          
+
+    case 'spotify':
+      spotSong(parameter);
+      break;
+
+    case 'movie':
+      movieInfo(parameter);
+      break;
+
+    case 'do-what-it-says':
+      getRandom();
+      break;
+
+      default:                            
+      logIt("Invalid Instruction");
+      break;
+
+  }
+};
+
